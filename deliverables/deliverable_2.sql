61ab919d27fd1c9bb77e8cbcf12774e7d3f29405
Patrick Crowe <pcrowe@hawk.iit.edu>
2020-11-11 21:04:45 -0600

    Debugged permissions. Runs successfully now

--- deliverables/deliverable_2.sql
+++ deliverables/deliverable_2.sql
@@ -4,18 +4,42 @@
 
  */
 
-create schema parking;
+--create schema parking;
 
 /* For debugging relation: To drop table, remove '--' and run the command */
 
---drop table if exists parking.parking_lot cascade;
---drop table if exists parking.spot cascade;
---drop table if exists parking.reservation;
---drop table if exists parking.temporary_license_plate;
---drop table if exists parking.employee cascade;
---drop table if exists parking.update_form;
---drop table if exists parking.member cascade;
---drop table if exists parking.user cascade;
+drop role if exists r_user;
+drop role if exists staff;
+drop role if exists admin;
+drop view if exists parking.booking;
+drop view if exists parking.mem_pay;
+drop view if exists parking.guest_pay;
+drop view if exists parking.lot_ratios;
+drop view if exists parking.times;
+drop table if exists parking.parking_lot cascade;
+drop table if exists parking.spot cascade;
+drop table if exists parking.reservation;
+drop table if exists parking.temporary_license_plate;
+drop table if exists parking.employee cascade;
+drop table if exists parking.update_form;
+drop table if exists parking.member cascade;
+drop table if exists parking.user cascade;
+
+create table parking.parking_lot (
+    lot_id char(1) not null,
+    guest_fee numeric(6,2),
+    membership_fee numeric(6,2),
+    upkeep_cost numeric(6,2),
+    location varchar(20),
+    primary key (lot_id)
+);
+
+create table parking.spot (
+    spot_id int not null,
+    lot_id char(1) not null,
+    primary key (spot_id, lot_id),
+    foreign key (lot_id) references parking.parking_lot (lot_id) on delete cascade on update cascade
+);
 
 create table parking.user (
     user_id varchar(20) not null,
@@ -42,7 +66,7 @@ create table parking.employee (
     password varchar(20) not null,
     salary numeric(8,2),
     type varchar(10) not null ,
-    check (type in ("staff", "admin")),
+    --check (type in ("staff", "admin")),
     primary key (employee_id)
 );
 
@@ -62,22 +86,6 @@ create table parking.update_form (
     new_value varchar(30),
     primary key (user_id, time_made),
     foreign key (user_id) references parking.user (user_id) on delete cascade
-);
-
-create table parking.parking_lot (
-    lot_id char(1) not null,
-    guest_fee numeric(6,2),
-    membership_fee numeric(6,2),
-    upkeep_cost numeric(6,2),
-    location varchar(20),
-    primary key (lot_id)
-);
-
-create table parking.spot (
-    spot_id int not null,
-    lot_id char(1) not null,
-    primary key (spot_id, lot_id),
-    foreign key (lot_id) references parking.parking_lot (lot_id) on delete cascade on update cascade
 );
 
 create table parking.reservation (
@@ -101,15 +109,48 @@ create table parking.reservation (
 alter table parking.member
 add foreign key (lot_id, spot_id) references parking.spot (lot_id, spot_id);
 
-create view parking.booking as select (reservation_time_in, reservation_time_out, spot_id, lot_id) from parking.reservation;
-create view parking.member_pay as select (lot_id, mem_count*membership_fee) from (select (lot_id, count(user_id) as mem_count) from parking.member group by lot_id) natural join parking.parking_lot;
-create view parking.guest_pay as select (lot_id, guest_count*guest_fee) from (select (lot_id, count(user_id, time_created) as guest_count) from parking.reservation where temporary = 1) natural join parking.parking_lot;
-create view parking.lot_ratios as (with total_lot_spots (lot_id, num_spot) as (select (lot_id, count(spot_id)) from parking.parking_lot group by lot_id),
-	with total_members (lot_id, num_mem) as (select (lot_id, count(user_id)) from parking.member group by lot_id),
-	with total_online (lot_id, num_onl) as (select (lot_id, count(user_id, time_created)) from parking.reservation where application_type = 'online' group by lot_id),
-	with total_drive_in (lot_id, num_dv) as (select (lot_id, count(user_id, time_created)) from parking.reservation where application_type = 'drive-in' group by lot_id)
-	select (lot_id, num_mem/num_spot, num_onl/num_spot, num_dv/num_spot) from total_lot_spots natural join total_members natural join total_online natural join total_drive_in);
-create view parking.times as select (user_id, login_time, logout_time) from parking.user;
+create view parking.booking as 
+	select reservation_time_in, reservation_time_out, spot_id, lot_id
+	from parking.reservation;
+create view parking.member_pay as 
+	select lot_id, mem_count*membership_fee 
+	from (
+		select * 
+		from (
+			select lot_id, count(user_id) as mem_count 
+			from parking.member group by lot_id
+		) as foo natural join parking.parking_lot
+	) as bar;
+create view parking.guest_pay as 
+	select lot_id, guest_count*guest_fee
+	from (
+		select *
+		from (
+			select lot_id, count(distinct (user_id, time_created)) as guest_count 
+			from parking.reservation where temporary = '1' group by lot_id
+		) as foo natural join parking.parking_lot
+	) as bar;
+create view parking.lot_ratios as (
+	with total_lot_spots (lot_id, num_spot) as (
+		select lot_id, count(spot_id) 
+		from parking.spot group by lot_id
+	),
+	total_members (lot_id, num_mem) as (
+		select lot_id, count(user_id)
+		from parking.member group by lot_id
+	),
+	total_online (lot_id, num_onl) as (
+		select lot_id, count(distinct (user_id, time_created))
+		from parking.reservation where application_type = 'online' group by lot_id
+	),
+	total_drive_in (lot_id, num_dv) as (
+		select lot_id, count(distinct(user_id, time_created)) 
+		from parking.reservation where application_type = 'drive-in' group by lot_id
+	)
+	select lot_id, num_mem/num_spot as ratio_mem, num_onl/num_spot as ratio_onl, num_dv/num_spot as ratio_dv
+	from total_lot_spots natural join total_members natural join total_online natural join total_drive_in
+);
+create view parking.times as select user_id, login_time, logout_time from parking.user;
 
 create role r_user;
 create role staff;
