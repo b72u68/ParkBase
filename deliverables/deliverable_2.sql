10c95f791d4575911c126994897e7ea8f705ca5b
b72u68 <mdinh@hawk.iit.edu>
2020-11-13 22:36:20 -0600

    Added constraints for application_type

--- deliverables/deliverable_2.sql
+++ deliverables/deliverable_2.sql
@@ -41,7 +41,7 @@ create table parking.spot (
 );
 
 create table parking.user (
-    user_id varchar(20) not null,
+    user_id char(7) not null,
     name varchar(30) not null,
     password varchar(20) not null,
     login_time timestamp,
@@ -50,7 +50,7 @@ create table parking.user (
 );
 
 create table parking.member (
-    user_id varchar(20) not null,
+    user_id char(7) not null,
     registered_license_plate char(7),
     lot_id char(1),
     spot_id int,
@@ -60,7 +60,7 @@ create table parking.member (
 );
 
 create table parking.employee (
-    employee_id varchar(20) not null,
+    employee_id char(7) not null,
     name varchar(30) not null,
     password varchar(20) not null,
     salary numeric(8,2),
@@ -71,7 +71,7 @@ create table parking.employee (
 
 /* TODO: Add remain_time() attribute */
 create table parking.temporary_license_plate (
-    user_id varchar(20) not null,
+    user_id char(7) not null,
     plate_number char(7),
     time_created timestamp,
     primary key (user_id, plate_number),
@@ -79,7 +79,7 @@ create table parking.temporary_license_plate (
 );
 
 create table parking.update_form (
-    user_id varchar(20) not null,
+    user_id char(7) not null,
     time_made timestamp,
     field_to_update varchar(20),
     new_value varchar(30),
@@ -88,14 +88,13 @@ create table parking.update_form (
 );
 
 create table parking.reservation (
-    user_id varchar(20) not null,
+    user_id char(7) not null,
     time_created timestamp,
     reservation_time_in timestamp,
     reservation_time_out timestamp,
     license_plate char(7),
-    application_type varchar(10),
-    temporary bit,
-    employee_id varchar(20) not null,
+    application_type varchar(10) check (application_type in ("online", "drive in", "member")),
+    employee_id char(7) not null,
     lot_id char(1) not null,
     spot_id int not null,
     primary key (user_id, time_created),
@@ -104,39 +103,35 @@ create table parking.reservation (
     foreign key (lot_id, spot_id) references parking.spot (lot_id, spot_id) on delete cascade on update cascade
 );
 
-/* Add foreign key spot (lot_id, spot_id) to member */
-alter table parking.member
-add foreign key (lot_id, spot_id) references parking.spot (lot_id, spot_id);
-
 --view for checking what spots are available at what time
-create view parking.booking as 											
+create view parking.booking as
 	select reservation_time_in, reservation_time_out, spot_id, lot_id
 	from parking.reservation;
-	
+
 --the following 4 tables are for running a report
 --member_pay and guest_pay will be combined to check the total revenue
-create view parking.member_pay as 
-	select lot_id, mem_count*membership_fee 
+create view parking.member_pay as
+	select lot_id, mem_count*membership_fee
 	from (
-		select * 
+		select *
 		from (
-			select lot_id, count(user_id) as mem_count 
+			select lot_id, count(user_id) as mem_count
 			from parking.member group by lot_id
 		) as foo natural join parking.parking_lot
 	) as bar;
-create view parking.guest_pay as 
+create view parking.guest_pay as
 	select lot_id, guest_count*guest_fee
 	from (
 		select *
 		from (
-			select lot_id, count(distinct (user_id, time_created)) as guest_count 
-			from parking.reservation where temporary = '1' group by lot_id
+			select lot_id, count(distinct (user_id, time_created)) as guest_count
+			from parking.reservation where application_type != 'member' group by lot_id
 		) as foo natural join parking.parking_lot
 	) as bar;
-	
+
 create view parking.lot_ratios as (
 	with total_lot_spots (lot_id, num_spot) as (
-		select lot_id, count(spot_id) 
+		select lot_id, count(spot_id)
 		from parking.spot group by lot_id
 	),
 	total_members (lot_id, num_mem) as (
@@ -148,7 +143,7 @@ create view parking.lot_ratios as (
 		from parking.reservation where application_type = 'online' group by lot_id
 	),
 	total_drive_in (lot_id, num_dv) as (
-		select lot_id, count(distinct(user_id, time_created)) 
+		select lot_id, count(distinct(user_id, time_created))
 		from parking.reservation where application_type = 'drive-in' group by lot_id
 	)
 	select lot_id, num_mem/num_spot as ratio_mem, num_onl/num_spot as ratio_onl, num_dv/num_spot as ratio_dv
