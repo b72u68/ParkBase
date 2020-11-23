7041b3b3be90fa6c16c3d44886808978c333771c
Patrick Crowe <pcrowe@hawk.iit.edu>
2020-11-23 15:31:25 -0600

    Touched up Login.java to branch for user/employee/admin

--- src/Login.java
+++ src/Login.java
@@ -57,20 +57,44 @@ public class Login extends JFrame{
 					stmt.setString(2, txtPassword.getText());
 					ResultSet rs = stmt.executeQuery();
 					if (rs.next()) {
-						UserMenu userMenu = new UserMenu(connection, "7463462");
-			            userMenu.requestUpdate();
 						setVisible(false); // HIDE THE FRAME
 						dispose(); // CLOSE OUT THE WINDOW
+						UserMenu userMenu = new UserMenu(connection, "7463462");
+			            userMenu.requestUpdate();
 					} else
 						lblStatus.setText("User not found");
-					while (rs.next()) {
-						System.out.println(rs.getString("user_id"));
-						System.out.println(rs.getString("password"));
-					}
 				} catch (SQLException ex) {
 					ex.printStackTrace();
 				}
-			}
+				query = "SELECT * FROM parking.employee WHERE parking.employee.employee_id = ? AND parking.employee.password = ?;";
+				try (PreparedStatement stmt = connection.prepareStatement(query)) {
+					stmt.setString(1, txtUname.getText());
+					stmt.setString(2, txtPassword.getText());
+					ResultSet rs = stmt.executeQuery();
+					if (rs.next()) {
+						setVisible(false); // HIDE THE FRAME
+						dispose(); // CLOSE OUT THE WINDOW
+						//Employee Menu
+					} else
+						lblStatus.setText("Employee not found");
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+				query = "SELECT * FROM parking.admin WHERE parking.admin.admin_id = ? AND parking.admin.password = ?;";
+				try (PreparedStatement stmt = connection.prepareStatement(query)) {
+					stmt.setString(1, txtUname.getText());
+					stmt.setString(2, txtPassword.getText());
+					ResultSet rs = stmt.executeQuery();
+					if (rs.next()) {
+						setVisible(false); // HIDE THE FRAME
+						dispose(); // CLOSE OUT THE WINDOW
+						//Admin Menu
+					} else
+						lblStatus.setText("Admin not found");
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}	
 		});
 		
 		btnExit.addActionListener(e -> System.exit(0));
