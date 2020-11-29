b6779b1fd568bf903c476067c4519fed2fe8f1a9
Danny Arvizu <darvizu061@gmail.com>
2020-11-29 11:19:01 -0600

    Connecting StaffMenu to login

--- src/Login.java
+++ src/Login.java
@@ -82,6 +82,7 @@ public class Login extends JFrame{
 								setVisible(false); // HIDE THE FRAME
 								dispose(); // CLOSE OUT THE WINDOW
 								//Employee Menu
+								new StaffMenu(getdbName(), getdbUsername(), getdbPassword());
 							} else
 								lblStatus.setText("Employee not found");
 						} catch (SQLException ex) {
